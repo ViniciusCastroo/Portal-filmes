be6aaa8e68142c9bb4cc9e3cf0e3a17d1c3b1199
ViniciusCastroo <161920909+ViniciusCastroo@users.noreply.github.com>
2024-09-26 20:03:35 -0300

    .

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
+
 export default function MovieListPage(){
+
+    const [contador, setContador] = useState(0)
+
+    const handleClick = () => {
+        setContador((prev) => prev + 1)
+        contador++
+        console.log(contador)
+    }
+
     return(
         <>
-        <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+            <p> {contador} </p>
+            <button onClick={handleClick}>Aumentar</button>
         </>
     )
 }
