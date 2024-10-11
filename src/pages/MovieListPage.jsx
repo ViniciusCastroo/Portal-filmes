97aa610cdb89e810140a80cd54fb61aab4b0161f
ViniciusCastroo <161920909+ViniciusCastroo@users.noreply.github.com>
2024-10-10 21:01:51 -0300

    .

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
 import movies from "../data/movies.json"
+import { data } from "autoprefixer"
 
 export default function MovieListPage() {
 
     const [search, setSearch] = useState("")
+    const[filmes, setfilmes] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br').then(res => res.json())
+        .then(data => setfilmes(data.results))
+        .catch(erro => console.log(erro))
+        .finally(() => console.log("Fim"))
+    },[])
+    
+
+    console.log(filmes)
 
     const handleSearch = (event) => {
         setSearch(event.target.value)
@@ -25,14 +37,14 @@ export default function MovieListPage() {
             />
             <section className="flex">
                 {
-                    filmesFiltrados.length > 0 ?
-
-                        filmesFiltrados
-                            .map(filme => (
-                                <MovieCard key={filme.id} {...filme} />
-                            ))
-                        :
-                        <p> Filme não encontrado</p>
+                   filmes.map(filme => (
+                    <>
+                        <h1>{filme.title}</h1>
+                        <p>{filme.vote_average}</p>
+                        <img src={`https://image.tmdb.org/t/p/w1280${filme.backdrop_path}`} alt={filme.title}/>
+                        <img src={`https://image.tmdb.org/t/p/w154${filme.poster_path}`} alt={filme.title}/>
+                    </>
+                   ))
                 }
             </section>
         </>
